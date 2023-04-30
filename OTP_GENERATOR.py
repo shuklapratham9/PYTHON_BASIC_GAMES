0b58ed8ae7400c0438ad64a2e81ad94e1366af29
shuklapratham9 <130244305+shuklapratham9@users.noreply.github.com>
2023-04-30 22:54:18 +0530

    Update OTP_GENERATOR.py

--- OTP_GENERATOR.py
+++ OTP_GENERATOR.py
@@ -1,5 +1,5 @@
 import random
 s=str(random.random())
-n=int(input("Enter the number of digits you want in your O.T.P: "))  #ENTER THE NUMBER OF DIGITS IN YHE O.T.P
+n=int(input("Enter the number of digits you want in your O.T.P: "))  #ENTER THE NUMBER OF DIGITS IN THE O.T.P
 print(s[-1:-1*(n+1):-1])
 
